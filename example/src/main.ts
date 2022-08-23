e45f195e214baf739ab15fff05cd7cf6068326a8
yangyi <297444172@qq.com>
2022-08-23 18:48:21 +0800

    fix: 编译css

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ZUi from "@z-ui/components";
 import '@z-ui/theme'
-import { Button } from "vue3-yytest";
 
 // import ZUi from "vue3-yytest";
-// import { ElButton } from 'element-plus/lib';
 // import test from "./test";
 // import 'vue3-yytest/es/style.css'
 // console.log('ZUi--',ZUi)
--- packages/build/buildStyle.ts
+++ packages/build/buildStyle.ts
@@ -8,31 +8,18 @@ import glob from "fast-glob"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const themeDir = resolve(__dirname, '../theme')
-const targetLib = resolve(__dirname, '../../dist/lib/theme')
-const targetEs = resolve(__dirname, '../../dist/es/theme')
+const targetDist = resolve(__dirname, '../../dist/theme')
 
 const buildSass = async () => {
-  //直接将sass文件复制到打包后目录
-  // await cpy(`${themeDir}/**/*.scss`, targetLib)
-  // await cpy(`${themeDir}/**/*.scss`, targetEs)
-  //获取打包后.scss文件目录(lib和es一样)
+  await cpy(`${themeDir}/**/*.scss`, targetDist)
   const sassFils = await glob("**/*.scss", { cwd: themeDir, onlyFiles: true })
-  // console.log('sassFils--',sassFils)
-  // return
-  //遍历含有sass的目录
   for (let path in sassFils) {
     const filePath = `${themeDir}/${sassFils[path]}`
-    console.log('filePath--',filePath)
-    // return
-    //获取sass文件字符串
-    const sassCode = await fs.readFile(filePath, 'utf-8')
-    //sass解析成css
-    const code = sass.compileString(sassCode)
-    //拿到.css后缀path
+    const code = await sass.compileAsync( filePath, {
+      style: "compressed"
+    })
     const cssPath = sassFils[path].replace('.scss', '.css')
-    //将css写入对应目录
-    await fs.writeFile(resolve(targetLib, cssPath), code.css)
-    await fs.writeFile(resolve(targetEs, cssPath), code.css)
+    await fs.writeFile(resolve(targetDist, cssPath), code.css)
   }
 }
 buildSass()
