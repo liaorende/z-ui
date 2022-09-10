cf9c08f80b8558f74db0c72961ed379695601f05
yangyi <297444172@qq.com>
2022-09-10 22:07:37 +0800

    fix: example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <test-a></test-a>
+  <!-- <test-a></test-a> -->
   <!-- <test-b></test-b> -->
+  <test-c
+    v-model:name="name"
+  ></test-c>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
 import TestA from './test1.vue'
 import TestB from './test2.vue'
+import TestC from './test3.vue'
+
+const name = ref('yy')
+
 </script>
 <style>
 body {
---
+++ example/src/test3.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{_name}}
+    {{_age}}
+    {{_sex}}
+  </div>
+  <z-button @click="_name='yangyi'">button</z-button>
+</template>
+
+<script setup lang="ts">
+import { useVModel } from '@vueuse/core'
+const props = defineProps({
+  name: String,
+  age: String,
+  sex: String
+})
+const emit = defineEmits(['update:name', 'update:age', 'update:sex'])
+
+const _name = useVModel(props, 'name', emit)
+const _age = useVModel(props, 'age', emit)
+const _sex = useVModel(props, 'sex', emit)
+
+</script>
+
+<style>
+</style>
